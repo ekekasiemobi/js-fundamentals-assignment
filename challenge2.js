aeb95c563b36a4f913ec8b86ff57abccd2c5cd49
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2025-12-17 01:35:24 +0100

    completed challenge2

---
+++ challenge2.js
@@ -0,0 +1,20 @@
+let num1 = 7
+let num2 = 5
+
+const addNum = num1 + num2 
+console.log("adition of "+ num1 +" and " + num2+" = "+addNum)
+
+const subNum = num1 - num2 
+console.log("subtraction of "+ num2 +" from " + num1+" = "+subNum)
+
+const multiplyNum = num1 * num2 
+console.log("multiplication  of "+ num1 +" by " + num2+" = "+multiplyNum)
+
+const divideNum = num1 / num2 
+console.log("division of "+ num1 +" by " + num2+" = "+divideNum)
+
+const modulusNum = num1 % num2 
+console.log("modulus of "+ num1 +" by " + num2+" = "+modulusNum)
+
+const expoNum = num1**num2 
+console.log("exponential of "+ num1 +" to " + num2+" = "+expoNum)
